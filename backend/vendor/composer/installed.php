4201d8d17997a7c6b45a546bf2ca30f80d1fab47
viniciusbeckerbernardini <vbkmma@gmail.com>
2022-02-10 19:54:07 -0300

    Pushing inital files

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '69503c351d579be3560bd752b9ab8497bd03d9da',
+        'name' => 'viniciusbeckerbernardini/desafio_leo_backend',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(
+                0 => '2.x-dev',
+            ),
+            'reference' => 'fb2c324c17941ffe805aa7c953895af96840d0c9',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+            ),
+        ),
+        'viniciusbeckerbernardini/desafio_leo_backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '69503c351d579be3560bd752b9ab8497bd03d9da',
+            'dev_requirement' => false,
+        ),
+    ),
+);
